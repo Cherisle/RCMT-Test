90341a849ef6ba922dc5090b45c0377363c16e7d
Rcchin <iraychin@gmail.com>
2016-09-23 15:00:40 -0700

    Add files via upload

---
+++ Testing2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 23 14:35:36 2016
+
+@author: irayc
+"""
+
+a = 4+3
+print(a)
+numbers = np.genfromtxt('Random10No.txt',delimiter=',')
+#my_file = open("C:\Users\irayc\Documents\PythonProject\")
+a = 4+3
+print(a)
+
+
+print(numbers)
+
